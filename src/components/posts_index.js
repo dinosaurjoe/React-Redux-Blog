b616efbcdfad2de16c251cedc77bb976c8e3bf7e
Joe Schafer <joeschaferastula@gmail.com>
2018-06-24 17:50:09 -0500

    populate props of post state and initial index render of posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts} from '../actions';
@@ -8,13 +9,29 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    _.map(this.prop.posts, post => {
+      return (
+          <li className="list-group-item" key={post.id}>
+            {post.title}
+          </li>
+        )
+    });
+  }
+
   render() {
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
 export default connect(null, { fetchPosts })(PostsIndex);
